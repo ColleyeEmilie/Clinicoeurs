9da74fb1c18e86a36c41999c5bb9f6c92debcb9c
emilie <colleyeemilie@hotmail.com>
2023-06-04 14:16:06 +0200

    :sparkles: add header.php language and menu

--- wp-content/themes/clinicoeurs/header.php
+++ wp-content/themes/clinicoeurs/header.php
@@ -2,27 +2,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, minimum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= get_bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="https://use.typekit.net/bcf2kyi.css">
-    <link rel="stylesheet" href="https://use.typekit.net/bcf2kyi.css">
-    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() . '/public/css/site.css'; ?>" />
-</head>
+    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() . '/public/css/site.css'; ?>" /></head>
 <body>
-
 <header class="header">
-    <h1 class="header__sitename hidden"><?= get_bloginfo('name'); ?></h1>
-    <p class="header__tagline hidden"><?= get_bloginfo('description'); ?></p>
-    <h2 class="hidden">Navigation</h2>
-    <input class="menu-btn" type="checkbox" id="menu-btn" />
-    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-    <ul class="menu">
-        <?php foreach(dwp_get_menu('main') as $link): ?>
-        <li>
-            <a href="<?= $link->href; ?>" class="nav__link">
-                <span class="nav__label"><?= $link->label; ?></span>
-            </a>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+    <h1 class="header__sitename"><?= get_bloginfo('name'); ?></h1>
+    <p class="header__tagline"><?= get_bloginfo('description'); ?></p>
 </header>
+
+<nav class="nav">
+    <?php foreach(clinicoeurs_get_menu('main') as $link): ?>
+        <a href="<?= $link->href; ?>" class="nav__link">
+            <span class="nav__label"><?= $link->label; ?></span>
+        </a>
+    <?php endforeach; ?>
+</nav>
+<div class="languages">
+    <ul class="languages__container">
+        <?php foreach(pll_the_languages(['raw'=>true]) as $lang):?>
+            <li class="languages__item <?= $lang['current_lang'] ? 'languages__link--current' : '' ?>">
+                <a href="<?= $lang['url'];?>" lang="<?= $lang['locale'];?>"  hreflang="<?= $lang['locale'];?>" class="languages__link"><?= $lang['name'];?></a>
+            </li>
+        <?php endforeach;?>
+    </ul>
+</div>
+
