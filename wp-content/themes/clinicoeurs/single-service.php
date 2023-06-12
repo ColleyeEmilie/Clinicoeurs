5f18d72a80d7109b35240db951fb18a14828d0a5
emilie <colleyeemilie@hotmail.com>
2023-06-12 18:10:05 +0200

    :sparkles: update single service

--- wp-content/themes/clinicoeurs/single-service.php
+++ wp-content/themes/clinicoeurs/single-service.php
@@ -1,5 +1,32 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
+<main class="service">
+    <h2 class="hidden"><?= get_the_title()?></h2>
+    <section class="service__presentation presentation1">
+        <div>
+            <h3>
 
+            </h3>
+            <p>
+
+            </p>
+            <h3>
+
+            </h3>
+            <p>
+
+            </p>
+        </div>
+        <figure>
+            <img src="" alt="">
+        </figure>
+    </section>
+
+    <section class="service__presentation presentation2">
+        <div>
+            <h3></h3>
+        </div>
+    </section>
+</main>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
