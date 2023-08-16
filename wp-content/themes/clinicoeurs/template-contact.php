6ef40eff51dd3e074bece60d1b911c7543718406
emilie <colleyeemilie@hotmail.com>
2023-08-16 10:44:14 +0200

    :sparkles: change form

--- wp-content/themes/clinicoeurs/template-contact.php
+++ wp-content/themes/clinicoeurs/template-contact.php
@@ -33,11 +33,11 @@
                                 <p>Merci&nbsp;! Votre message a bien été envoyé.</p>
                             </div>
                         <?php endif; ?>
-                            <?php if($errors): ?>
-                                <div class="error">
-                                    <p>Attention&nbsp;! Merci de corriger les erreurs du formulaire.</p>
-                                </div>
-                            <?php endif; ?>
+                        <?php if($errors): ?>
+                            <div class="error">
+                                <p>Attention&nbsp;! Merci de corriger les erreurs du formulaire.</p>
+                            </div>
+                        <?php endif; ?>
 
                             <form action="<?= esc_url(admin_url('admin-post.php')); ?>" method="POST" class="contact__form">
                                 <fieldset class="contact__info">
@@ -91,7 +91,6 @@
                                     <button class="contact__submit" type="submit">Envoyer</button>
                                 </div>
                             </form>
-                        <?php endif; ?>
                     </div>
                 </section>
             </div>
